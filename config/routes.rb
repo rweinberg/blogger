562ef69422e881d62d086d085b11707ecc85d576
rweinberg <rweinberg@cmu.edu>
2013-07-29 15:39:23 +0000

    comments features

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Blogger::Application.routes.draw do
   
-  resources :articles
+  resources :articles do
+    resources :comments   #use comments as a subresource
+  end
+  
   
   root to: 'articles#index'
+  
+  
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
